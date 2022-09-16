9bcaa801a24d68478301693230eb80a686e504a2
dmitriy <dmitbely@protonmail.ch>
2022-09-16 21:26:06 +0300

    first examples

---
+++ a_to_b/Program.cs
@@ -0,0 +1,23 @@
+﻿// Собрать строку с числами от a до b, a <= b
+
+string NumberFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++) // если нужно выводить значения
+    // от большего к меньшему: i>= b; i--
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumberRec(int a, int b)
+{
+    if(a <= b) return $"{a} " + NumberRec(a + 1, b);// если нужно выводить значения
+    // от большего к меньшему: NumberRec(a + 1, b) + $"{a}";
+    else return String.Empty;
+}
+
+Console.Write(NumberFor(1, 10));
+Console.WriteLine(String.Empty);
+Console.Write(NumberRec(1, 10));
---
+++ directories/Program.cs
@@ -0,0 +1,9 @@
+﻿string path = @"/Документы/обучение/C#/C#/Lections/";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime); // время создани директории
+FileInfo[] fi =  di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
