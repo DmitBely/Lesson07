4c775606e4faeceb90aa57158300b5ead5f7bb1d
dmitriy <dmitbely@protonmail.ch>
2022-09-16 21:52:58 +0300

    trees

---
+++ trees/Program.cs
@@ -0,0 +1,19 @@
+﻿// ((4-2) * (1 + 3)) / 10
+
+string emp = String.Empty;
+string[] tree = {emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+//                0    1    2     3    4    5    6    7    8    9    10   11
+
+void InOrdeerTraversal(int pos = 1)
+{
+    if(pos< tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrdeerTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrdeerTraversal(right);
+    }
+}
+
+InOrdeerTraversal();
